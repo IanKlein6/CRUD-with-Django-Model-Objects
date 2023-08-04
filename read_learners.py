1a3a266feece02e8a758220d68542a355fc3c164
IanKlein6 <ianklein66@gmail.com>
2023-08-04 13:16:50 +0200

    commit

---
+++ read_learners.py
@@ -0,0 +1,27 @@
+# Django specific settings
+import inspect
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+from django.db import connection
+# Ensure settings are read
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+
+from crud.models import *
+from datetime import date
+
+
+ # Find students with last name "Smith"
+learners_smith = Learner.objects.filter(last_name="Smith")
+print("1. Find learners with last name `Smith`")
+print(learners_smith)
+print("\n")
+# Order by dob descending, and select the first two objects
+learners_youngest = Learner.objects.order_by('-dob')[0:2]
+print("2. Find top two youngest learners")
+print(learners_youngest)
+
+#All learners age from youngest to oldest
+learners_all_by_age = Learner.objects.order_by('-dob')
+print("All Learners from youngest to oldest")
+print(learners_all_by_age)
